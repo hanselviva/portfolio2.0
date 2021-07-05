922f4de835abaa02644af7f9aff1965f268ea6a6
HanselV <hanselviva@gmail.com>
2021-07-04 23:36:12 -0700

    added new proj

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,28 +19,37 @@ import { Parallax } from "react-parallax";
 
 const projects = [
 	{
-		name: "Make-your-own Pizza",
-		image:
-			"https://cdn.pixabay.com/photo/2017/12/05/20/10/pizza-3000285_960_720.png",
+		name: "Rent-A-Game",
+		image: "https://i.ibb.co/XpF72td/Screenshot-7.png",
 		description:
-			"My first ever deployed app, complete from Form Management & Validation to routing, post requests, and Cypress testing. Basic styling is for comprehensibility purposes.",
+			"A fully functional rental store built on React with Redux state management. This app has also client authentication using Axios. The database API is pulled from FreeToGame.com which I have tweaked to cater the needs of the app.",
 		role: "Personal Project",
-
-		demo: "https://gallant-hopper-0e9073.netlify.app/",
-		code: "https://github.com/hanselviva/pizza-eats",
-		tools: "React, Axios, react-router-dom, Yup, Cypress",
+		demo: "https://store-app-mu.vercel.app/",
+		code: "https://github.com/hanselviva/react-async-redux",
+		tools: "React, Redux, Redux Thunk, Material-UI, more..",
 	},
-
 	{
 		name: "Water My Plants",
 		image:
 			"https://cdn.pixabay.com/photo/2018/03/02/09/19/flower-marigolds-3192686_960_720.png",
 		description:
-			"Built alongside a team of Lambda School Web Development students. 'Water My Plants' reminds users to water their plants. The app is complete from frontend to backend functionalities.",
-		role: "Front-end, built the components",
+			"Built alongside a team of Lambda School WebDev students in an AGILE environment. 'Water My Plants' reminds users to water their plants. The app is complete from frontend to backend functionalities.",
+		role: "Front-end; form validation, components styling & functionalities",
 		demo: "https://water-my-plants-tt157.vercel.app/",
 		code: "https://github.com/Build-Week-Water-My-Plants-157",
-		tools: "React, Redux, Axios, Material-UI, react-router-dom, Yup ",
+		tools: "React, Redux, Axios, Material-UI, react-router-dom, Yup",
+	},
+	{
+		name: "Make-your-own Pizza",
+		image:
+			"https://cdn.pixabay.com/photo/2017/12/05/20/10/pizza-3000285_960_720.png",
+		description:
+			"My first ever deployment, complete Form Management from validation to routing, post requests, and Cypress testing. Basic styling is added only for comprehensibility.",
+		role: "Personal Project",
+
+		demo: "https://gallant-hopper-0e9073.netlify.app/",
+		code: "https://github.com/hanselviva/pizza-eats",
+		tools: "React, Axios, react-router-dom, Yup, Cypress",
 	},
 	{
 		name: "React Portfolio",
@@ -55,11 +64,11 @@ const projects = [
 	{
 		name: "Simple React Calculator",
 		image: "https://i.ibb.co/SK38Xnj/Screenshot-5.png",
-		description: "Simple React calculator using actions and reducers.",
+		description: "Simple React calculator using actions and reducers",
 		role: "Personal Project",
 		demo: "https://reducer-calculator.netlify.app/",
 		code: "https://github.com/hanselviva/react-calculator",
-		tools: "React, useReducer",
+		tools: "React with actions and reducers",
 	},
 	{
 		name: "Jokes Generator",
@@ -127,6 +136,9 @@ const useStyles = makeStyles((theme) => ({
 		marginTop: theme.spacing(1),
 		marginBottom: theme.spacing(10),
 	},
+	gridRoot: {
+		flexGrow: 1,
+	},
 }));
 
 const Projects = () => {
@@ -138,68 +150,70 @@ const Projects = () => {
 					<Typed strings={["PROJECTS"]} typeSpeed={40} />
 				</Typography>
 
-				<Grid container spacing={4} className={classes.cardGrid}>
-					{/* <Grid container spacing={4} className={classes.cardGrid} maxWidth="md"> */}
+				<div className={classes.gridRoot}>
+					<Grid container spacing={6} className={classes.cardGrid}>
+						{/* <Grid container spacing={4} className={classes.cardGrid} maxWidth="md"> */}
 
-					{projects.map((card, i) => (
-						<Grid item key={i} md={4} className={classes.cardWrapper}>
-							<Parallax
-								renderLayer={(percentage) => (
-									<Card
-										className={classes.card}
-										style={{
-											background: `rgba(
+						{projects.map((card, i) => (
+							<Grid item key={i} md={4} className={classes.cardWrapper}>
+								<Parallax
+									renderLayer={(percentage) => (
+										<Card
+											className={classes.card}
+											style={{
+												background: `rgba(
 												0,
 												${percentage * 101},
 												${percentage * 120}, ${percentage * 1})`,
-										}}
-									>
-										<CardMedia
-											className={classes.cardMedia}
-											image={card.image}
-											style={{ opacity: `${percentage * 1}` }}
-										/>
-										<CardContent className={classes.cardContent}>
-											<Typography gutterBottom variant="h5" component="h2">
-												{card.name}
-											</Typography>
-											<Typography className={classes.cardContentDesc}>
-												<b> Role: </b> {card.role} <br />
-											</Typography>
-											<Typography className={classes.cardContentDesc}>
-												<b> Description: </b> {card.description}
-											</Typography>
-											<Typography className={classes.cardContentDesc}>
-												<b> Tools: </b> {card.tools} <br />
-											</Typography>
-										</CardContent>
-										<CardActions>
-											<a rel="noreferrer" target="_blank" href={card.demo}>
-												<Button
-													size="small"
-													color="primary"
-													variant="contained"
-												>
-													Demo
-												</Button>
-											</a>
+											}}
+										>
+											<CardMedia
+												className={classes.cardMedia}
+												image={card.image}
+												style={{ opacity: `${percentage * 1}` }}
+											/>
+											<CardContent className={classes.cardContent}>
+												<Typography gutterBottom variant="h5" component="h2">
+													{card.name}
+												</Typography>
+												<Typography className={classes.cardContentDesc}>
+													<b> Role: </b> {card.role} <br />
+												</Typography>
+												<Typography className={classes.cardContentDesc}>
+													<b> Description: </b> {card.description}
+												</Typography>
+												<Typography className={classes.cardContentDesc}>
+													<b> Tools: </b> {card.tools} <br />
+												</Typography>
+											</CardContent>
+											<CardActions>
+												<a rel="noreferrer" target="_blank" href={card.demo}>
+													<Button
+														size="small"
+														color="primary"
+														variant="contained"
+													>
+														Demo
+													</Button>
+												</a>
 
-											<a rel="noreferrer" target="_blank" href={card.code}>
-												<Button
-													size="small"
-													color="primary"
-													variant="contained"
-												>
-													View Code
-												</Button>
-											</a>
-										</CardActions>
-									</Card>
-								)}
-							></Parallax>
-						</Grid>
-					))}
-				</Grid>
+												<a rel="noreferrer" target="_blank" href={card.code}>
+													<Button
+														size="small"
+														color="primary"
+														variant="contained"
+													>
+														View Code
+													</Button>
+												</a>
+											</CardActions>
+										</Card>
+									)}
+								></Parallax>
+							</Grid>
+						))}
+					</Grid>
+				</div>
 			</Container>
 		</Container>
 	);
